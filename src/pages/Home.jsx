8c840f2912a659623f271398ff60a412d04f9f54
Your Name <ronitgandhi96@gmail.com>
2022-08-20 20:39:16 +0530

    minor fix.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
 			<Row title="Popular" fetchURL={requests.requestPopular} />
 			<Row title="Trending" fetchURL={requests.requestTrending} />
 			<Row title="Top Rated" fetchURL={requests.requestTopRated} />
-			<Row title="Horror" fetchURL={requests.requestHorror} />
 		</>
 	);
 };
