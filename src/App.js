673ebe7e30dca83437b8143ac514e318d2e1a4f0
Ronit-Linux <ronitgandhi@Hp-Laptop>
2022-08-20 17:32:02 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { AuthContextProvider } from "./Context/AuthContext";
+import Account from "./Pages/Account";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<AuthContextProvider>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/account" element={<Account />} />
+				</Routes>
+			</AuthContextProvider>
+		</>
+	);
+};
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "../Components/Main";
+import Row from "../Components/Row";
+import requests from "../Requests";
+
+const Home = () => {
+	return (
+		<>
+			<Main />
+			<Row title="Up Coming" fetchURL={requests.requestUpcoming} />
+			<Row title="Popular" fetchURL={requests.requestPopular} />
+			<Row title="Trending" fetchURL={requests.requestTrending} />
+			<Row title="Top Rated" fetchURL={requests.requestTopRated} />
+			<Row title="Horror" fetchURL={requests.requestHorror} />
+		</>
+	);
+};
+
+export default Home;
